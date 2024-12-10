482628c4a89703bc6f2abc01c5882528489b95ad
wasabina67 <wasabina67@gmail.com>
2024-12-10 13:44:09 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask
+import io
+import os
+
+from flask import Flask, send_file
+from openpyxl import load_workbook  # type: ignore
 
 app = Flask(__name__)
+
+
+@app.route("/download_xlsx")
+def download_xlsx():
+    return
 
 
 if __name__ == "__main__":
